a22e60973ff469dc41aab3af9ef004dbba0c257c
vuone <70443590+vuone@users.noreply.github.com>
2023-05-03 11:54:56 +0545

    Update Tickers.py

--- pages/Tickers.py
+++ pages/Tickers.py
@@ -67,6 +67,13 @@ if not df_filtered.empty:
         color_discrete_sequence=["#0083B8"],
         template="plotly_white"
     )
+    fig_paidup2.update_layout(
+    # Disable zooming and panning
+    dragmode=False,
+    uirevision="True",
+    xaxis={"fixedrange": True},
+    yaxis={"fixedrange": True},
+)
 
  # Create a subplot with two y-axes
     fig_capeps = make_subplots(specs=[[{"secondary_y": True}]])
