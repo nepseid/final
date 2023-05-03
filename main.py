7464d2b91bdc2a0d0b810f9a2c0cce541d37abc4
vuone <70443590+vuone@users.noreply.github.com>
2023-05-03 11:17:09 +0545

    Update main.py

--- main.py
+++ main.py
@@ -64,9 +64,6 @@ fig_price = px.bar(
     title="<b>Last Traded Price </b>",
     color_discrete_sequence=["#0083B8"],
     template="plotly_white",
-    config={
-        "scrollZoom": False
-    }
 )
 
 paid_up = (
