343febccbef40dd14292bbf5b81a86e96657d904
vuone <70443590+vuone@users.noreply.github.com>
2023-05-03 10:23:49 +0545

    Update main.py

--- main.py
+++ main.py
@@ -64,6 +64,10 @@ fig_price = px.bar(
     title="<b>Last Traded Price </b>",
     color_discrete_sequence=["#0083B8"],
     template="plotly_white",
+),
+config={
+        "scrollZoom": False  # Remove the zoom functionality
+    }
 )
 
 
