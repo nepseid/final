cbd27b8ac4bc0b42a79cff62276dd13eb2bff652
vuone <70443590+vuone@users.noreply.github.com>
2023-05-16 18:46:37 +0545

    Update Tickers.py

--- pages/Tickers.py
+++ pages/Tickers.py
@@ -3,6 +3,7 @@ import plotly.express as px
 import streamlit as st
 from plotly.subplots import make_subplots
 import plotly.graph_objects as go
+import numpy as np
 
 st.set_page_config(
     page_title="Scrip Analysis",
@@ -71,6 +72,7 @@ if not df_filtered.empty:
         color_discrete_sequence=["#0083B8"],
         template="plotly_white"
     )
+
     # Create a subplot with two y-axes
     fig_capeps = make_subplots(specs=[[{"secondary_y": True}]])
 
@@ -94,20 +96,19 @@ if not df_filtered.empty:
     fig_capeps.add_trace(
         go.Scatter(
             x=df_filtered["Timeframe"],
-            y=df_filtered["PAID-UP"]*1000,
+            y=df_filtered["PAID-UP"] * 1000,
             mode="lines",
             name="Capital",
             line=dict(color="#FBB13C")
         ),
         secondary_y=True
     )
-
     # Set the axis labels and titles
-    fig_capeps.update_xaxes(title_text="Timeframe")
-    fig_capeps.update_yaxes(title_text="EPS", secondary_y=False)
-    fig_capeps.update_yaxes(title_text="PAID-UP", secondary_y=True)
+fig_capeps.update_xaxes(title_text="Timeframe")
+fig_capeps.update_yaxes(title_text="EPS", secondary_y=False)
+fig_capeps.update_yaxes(title_text="PAID-UP", secondary_y=True)
 
-    # Set the figure title
+# Set the figure title
 fig_capeps.update_layout(
     title=f"<b>EPS and Capital for {symbol}, {quarter}</b>"
 )
@@ -115,22 +116,49 @@ fig_eps2.update_layout(showlegend=False)
 fig_eps2.update_traces(
     hovertemplate='<br>EPS: %{y}',
     hoverlabel=dict(namelength=0),
-    texttemplate=''
+    texttemplate='%{y:.2s}',  # Format values in thousands, millions, billions
+    textposition='auto'  # Show values on the bars
 )
+
 fig_paidup2.update_layout(showlegend=False)
 fig_paidup2.update_traces(
     hovertemplate='<br>Capital: %{y}',
     hoverlabel=dict(namelength=0),
-    texttemplate=''
+    texttemplate='%{y}',  # Format values in thousands, millions, billions
+    textposition='auto'  # Show values on the bars
 )
 fig_roe.update_layout(showlegend=False)
 fig_roe.update_traces(
     hovertemplate='<br>ROE: %{y}',
     hoverlabel=dict(namelength=0),
-    texttemplate=''
+    # Format values in thousands, millions, billions
+    texttemplate='%{y}',
+    textposition='auto'  # Show values on the bars
 )
 fig_capeps.update_layout(showlegend=False)
 
+# Filter the dataframe for NPL chart (with the latest year at index 6)
+# Get the latest year from the unique values
+latest_year = df1["Year"].unique()[-1]
+df_npl = df1[(df1["SYMBOL"] == symbol) & (df1["Year"] == latest_year)]
+
+if not df_npl.empty:
+    fig_npl = px.bar(
+        df_npl,
+        x="Quarter",
+        y="NPL",
+        title=f"<b>NPL for {symbol}, {df_npl['Year'].iloc[-1]}</b>",
+        color_discrete_sequence=["#0083B8"],
+        template="plotly_white"
+    )
+
+    fig_npl.update_traces(
+        hovertemplate='<br>NPL: %{y}',
+        hoverlabel=dict(namelength=0),
+        # Format values in thousands, millions, billions
+        texttemplate='%{y:.2s}',
+        textposition='auto'  # Show values on the bars
+    )
 
 # Filter the dataframe for cash and bonus combo chart (without quarter filter)
 df_filtered_combo = df1[df1["SYMBOL"] == symbol]
@@ -140,42 +168,26 @@ if not df_filtered_combo.empty:
     avg_cash_bonus = df_filtered_combo.groupby("Year").agg(
         {"Bonus": "mean", "Cash": "mean"}).reset_index()
 
-    # Create a combo bar chart with average values
-    fig_combo = make_subplots(specs=[[{"secondary_y": True}]])
-
-    # Add a bar chart with average bonus values to the primary y-axis
-    fig_combo.add_trace(
-        go.Bar(
-            x=avg_cash_bonus["Year"],
-            y=avg_cash_bonus["Bonus"],
-            name="Bonus",
-            marker_color="#0083B8",
-            hovertemplate="Year: %{x}<br>Bonus: %{y}"
-        ),
-        secondary_y=False
-    )
-
-    # Add a line chart with average cash values to the secondary y-axis
-    fig_combo.add_trace(
-        go.Scatter(
-            x=avg_cash_bonus["Year"],
-            y=avg_cash_bonus["Cash"],
-            name="Cash",
-            line=dict(color="#FBB13C"),
-            hovertemplate="Year: %{x}<br>Cash: %{y}"
-        ),
-        secondary_y=True
-    )
-
-    # Set the axis labels and titles
-    fig_combo.update_xaxes(title_text="Year")
-    fig_combo.update_yaxes(title_text="Bonus", secondary_y=False)
-    fig_combo.update_yaxes(title_text="Cash", secondary_y=True)
-
-    # Set the figure title
-    fig_combo.update_layout(
+    # Create a stacked column chart with average values
+    fig_combo = px.bar(
+        avg_cash_bonus,
+        x="Year",
+        y=["Bonus", "Cash"],
         title=f"<b>Bonus Dividend {symbol}</b>",
-        showlegend=False
+        barmode="stack",
+        color_discrete_sequence=["#0083B8", "#FBB13C"],
+        template="plotly_white"
+    )
+
+    fig_combo.update_layout(showlegend=False)
+    fig_combo.update_traces(
+        hovertemplate='Year: %{x}<br>Bonus: %{customdata[0]}<br>Cash: %{customdata[1]}',
+        hoverlabel=dict(namelength=0),
+        # Format values in thousands, millions, billions
+        texttemplate='%{y}',
+        textposition='auto',  # Show values on the bars
+        customdata=np.stack(
+            (avg_cash_bonus["Bonus"], avg_cash_bonus["Cash"]), axis=-1)
     )
 
     # Create a combo bar chart with average values
@@ -185,6 +197,9 @@ if not df_filtered_combo.empty:
     st.plotly_chart(fig_roe, use_container_width=True)
     st.plotly_chart(fig_paidup2, use_container_width=True)
     st.plotly_chart(fig_capeps, use_container_width=True)
-
+    if fig_npl is not None:
+        st.plotly_chart(fig_npl, use_container_width=True)
+    else:
+        st.write("No NPL data available for the selected symbol and the latest year.")
 else:
     st.write("No data available for the selected symbol.")
