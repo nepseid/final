9b05c1b6660764c918963ebeeb648f892c0bb1d5
vuone <70443590+vuone@users.noreply.github.com>
2024-01-26 13:55:41 +0545

    Update main.py

--- main.py
+++ main.py
@@ -15,9 +15,9 @@ Sector = st.sidebar.selectbox(
     "Sector:", options=sorted(df["Sector"].unique()), index=0)
 df_sector = df.query("Sector == @Sector")
 
-Year = st.sidebar.selectbox("Year", sorted(df["Year"].unique()), index=6)
+Year = st.sidebar.selectbox("Year", sorted(df["Year"].unique()), index=7)
 Quarter = st.sidebar.selectbox(
-    "Quarter", sorted(df["Quarter"].unique()), index=3)
+    "Quarter", sorted(df["Quarter"].unique()), index=1)
 SYMBOL = st.sidebar.multiselect("Scips:", options=df_sector["SYMBOL"].unique(
 ), default=df_sector["SYMBOL"].unique())
 df_selection = df.query(
