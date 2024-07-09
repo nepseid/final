4887e95d1112638fff9940d365db21218c7fcbe0
vuone <70443590+vuone@users.noreply.github.com>
2024-07-09 08:59:27 +0545

    Update main.py

--- main.py
+++ main.py
@@ -23,157 +23,151 @@ SYMBOL = st.sidebar.multiselect("Scips:", options=df_sector["SYMBOL"].unique(
 df_selection = df.query(
     "Sector==@Sector & Year==@Year & Quarter==@Quarter & SYMBOL==@SYMBOL")
 
-# Debug prints
-st.write("Columns in df_selection:", df_selection.columns)
-st.write("df_selection after filtering:", df_selection)
 
-# Check if df_selection is empty
-if df_selection.empty:
-    st.error("The filtered DataFrame 'df_selection' is empty. Please adjust your filters.")
-else:
-    # Proceed with the rest of the code
-    def format_value(value):
-        if value >= 1e9:
-            return f"{value/1e9:.2f}B"
-        elif value >= 1e6:
-            return f"{value/1e6:.2f}M"
-        elif value >= 1e3:
-            return f"{value/1e3:.2f}K"
-        else:
-            return f"{value:.2f}"
+def format_value(value):
+    if value >= 1e9:
+        return f"{value/1e9:.2f}B"
+    elif value >= 1e6:
+        return f"{value/1e6:.2f}M"
+    elif value >= 1e3:
+        return f"{value/1e3:.2f}K"
+    else:
+        return f"{value:.2f}"
 
-    def create_bar_chart(data, x, y, title):
-        data["v"] = data[y].apply(format_value)
-        fig = px.bar(
-            data,
-            x=x,
-            y=y,
-            text=data["v"],
-            orientation="v",
-            title=f"<b>{title}</b>",
-            color_discrete_sequence=["#0083B8"] * len(data),
-            template="plotly_white"
-        )
-        fig.update_traces(textposition="outside")
 
-        # Disable zooming
-        fig.update_layout(
-            dragmode=False,
-            xaxis=dict(fixedrange=True),
-            yaxis=dict(fixedrange=True)
-        )
+def create_bar_chart(data, x, y, title):
+    data["v"] = data[y].apply(format_value)
+    fig = px.bar(
+        data,
+        x=x,
+        y=y,
+        text=data["v"],
+        orientation="v",
+        title=f"<b>{title}</b>",
+        color_discrete_sequence=["#0083B8"] * len(data),
+        template="plotly_white"
+    )
+    fig.update_traces(textposition="outside")
 
-        return fig
-
-    # Last Traded Price
-    price = df_selection.groupby(by=["SYMBOL"]).sum()[
-        ["Price"]].sort_values(by="Price")
-    fig_price = create_bar_chart(price, price.index, "Price", "Last Traded Price")
-    fig_price.update_traces(hovertemplate="<b>Price:</b> %{y}")
-
-    # Capital Framework
-    paid_up = df_selection.groupby(by=["SYMBOL"]).sum()[
-        ["PAID-UP"]].sort_values(by="PAID-UP")
-    paid_up["PAID-UP"] *= 1000  # Multiply the "PAID-UP" values by 1000
-    fig_paid_up = create_bar_chart(
-        paid_up, paid_up.index, "PAID-UP", "Capital Framework")
-    fig_paid_up.update_traces(hovertemplate="<b>Capital:</b> %{y}")
-
-    # EPS
-    eps = df_selection.groupby(by=["SYMBOL"]).sum()[["EPS"]].sort_values(by="EPS")
-    fig_eps = create_bar_chart(eps, eps.index, "EPS", "EPS")
-    fig_eps.update_traces(hovertemplate="<b>EPS:</b> %{y}")
-
-    # Book Value
-    bookvalue = df_selection.groupby(by=["SYMBOL"]).sum()[
-        ["BOOK VALUE"]].sort_values(by="BOOK VALUE")
-    fig_bookvalue = create_bar_chart(
-        bookvalue, bookvalue.index, "BOOK VALUE", "Book Value")
-    fig_bookvalue.update_traces(hovertemplate="<b>Book Value:</b> %{y}")
-
-    # DPS
-    dps = df_selection.groupby(by=["SYMBOL"]).sum()[["Dps"]].sort_values(by="Dps")
-    fig_dps = create_bar_chart(dps, dps.index, "Dps", "DPS")
-    fig_dps.update_traces(hovertemplate="<b>DPS:</b> %{y}")
-
-    # Net Profit
-    netprofit = df_selection.groupby(by=["SYMBOL"]).sum()[
-        ["NET PROFIT"]].sort_values(by="NET PROFIT")
-    netprofit["NET PROFIT"] *= 1000  # Multiply the "Net Profit" values by 1000
-    fig_netprofit = create_bar_chart(
-        netprofit, netprofit.index, "NET PROFIT", "Net Profit")
-    fig_netprofit.update_traces(hovertemplate="<b>Net Profit:</b> %{y}")
-
-    # PE Ratio
-
-    pe = df_selection.groupby(by=["SYMBOL"]).sum()[["PE"]].sort_values(by="PE")
-    fig_pe = create_bar_chart(pe, pe.index, "PE", "PE Ratio")
-    fig_pe.update_traces(hovertemplate="<b>PE:</b> %{y}")
-
-    # NPL
-
-    npl = df_selection.groupby(by=["SYMBOL"]).sum()[["NPL"]].sort_values(by="NPL")
-    fig_npl = create_bar_chart(npl, npl.index, "NPL", "NPL")
-    fig_npl.update_traces(hovertemplate="<b>NPL:</b> %{y}")
-
-    # ROE 
-
-    roe = df_selection.groupby(by=["SYMBOL"]).sum()[["ROE"]].sort_values(by="ROE")
-    fig_roe = create_bar_chart(roe, roe.index, "ROE", "ROE")
-    fig_roe.update_traces(hovertemplate="<b>ROE:</b> %{y}")
-
-    # EPS and DPS
-
-    eps_dps = df_selection.groupby(by=["SYMBOL"]).sum()[
-        ["EPS", "Dps"]].sort_values(by="EPS")
-    fig_eps_dps = go.Figure()
-    fig_eps_dps.add_trace(go.Bar(
-        x=eps_dps.index,
-        y=eps_dps["EPS"],
-        name="EPS",
-        marker_color="#0083B8",
-        hovertemplate="<b>EPS:</b> %{y}<extra></extra>",
-        text=eps_dps["EPS"],
-        textposition="auto",
-    ))
-    fig_eps_dps.add_trace(go.Bar(
-        x=eps_dps.index,
-        y=eps_dps["Dps"],
-        name="DPS",
-        marker_color="#FFA15A",
-        hovertemplate="<b>DPS:</b> %{y}<extra></extra>",
-        text=eps_dps["Dps"],
-        textposition="auto",
-    ))
-    fig_eps_dps.update_layout(
-        title="<b>EPS and DPS</b>",
-        template="plotly_white",
-        xaxis_title="Symbol",
-        yaxis_title="Value",
-        legend=dict(x=0.7, y=1),
-
-        # Disable zooming
+    # Disable zooming
+    fig.update_layout(
         dragmode=False,
         xaxis=dict(fixedrange=True),
         yaxis=dict(fixedrange=True)
     )
-    # Public Shares
 
-    ps = df_selection.groupby(by=["SYMBOL"]).sum(
-    )[[ "Public Shares"]].sort_values(by="Public Shares")
-    fig_ps = create_bar_chart(ps, ps.index, "Public Shares", "Public Shares")
-    fig_ps.update_traces(hovertemplate="<b>Public Shares:</b> %{y}")
+    return fig
 
-    # Display charts
 
-    st.plotly_chart(fig_price, use_container_width=True)
-    st.plotly_chart(fig_ps, use_container_width=True)
-    st.plotly_chart(fig_paid_up, use_container_width=True)
-    st.plotly_chart(fig_eps_dps, use_container_width=True)
-    st.plotly_chart(fig_bookvalue, use_container_width=True)
-    st.plotly_chart(fig_pe, use_container_width=True)
-    st.plotly_chart(fig_netprofit, use_container_width=True)
-    st.plotly_chart(fig_roe, use_container_width=True)
-    st.plotly_chart(fig_npl, use_container_width=True)
-    st.plotly_chart(fig_eps, use_container_width=True)
-    st.plotly_chart(fig_dps, use_container_width=True)
+# Last Traded Price
+price = df_selection.groupby(by=["SYMBOL"]).sum()[
+    ["Price"]].sort_values(by="Price")
+fig_price = create_bar_chart(price, price.index, "Price", "Last Traded Price")
+fig_price.update_traces(hovertemplate="<b>Price:</b> %{y}")
+
+# Capital Framework
+paid_up = df_selection.groupby(by=["SYMBOL"]).sum()[
+    ["PAID-UP"]].sort_values(by="PAID-UP")
+paid_up["PAID-UP"] *= 1000  # Multiply the "PAID-UP" values by 1000
+fig_paid_up = create_bar_chart(
+    paid_up, paid_up.index, "PAID-UP", "Capital Framework")
+fig_paid_up.update_traces(hovertemplate="<b>Capital:</b> %{y}")
+
+# EPS
+eps = df_selection.groupby(by=["SYMBOL"]).sum()[["EPS"]].sort_values(by="EPS")
+fig_eps = create_bar_chart(eps, eps.index, "EPS", "EPS")
+fig_eps.update_traces(hovertemplate="<b>EPS:</b> %{y}")
+
+# Book Value
+bookvalue = df_selection.groupby(by=["SYMBOL"]).sum()[
+    ["BOOK VALUE"]].sort_values(by="BOOK VALUE")
+fig_bookvalue = create_bar_chart(
+    bookvalue, bookvalue.index, "BOOK VALUE", "Book Value")
+fig_bookvalue.update_traces(hovertemplate="<b>Book Value:</b> %{y}")
+
+# DPS
+dps = df_selection.groupby(by=["SYMBOL"]).sum()[["Dps"]].sort_values(by="Dps")
+fig_dps = create_bar_chart(dps, dps.index, "Dps", "DPS")
+fig_dps.update_traces(hovertemplate="<b>DPS:</b> %{y}")
+
+# Net Profit
+netprofit = df_selection.groupby(by=["SYMBOL"]).sum()[
+    ["NET PROFIT"]].sort_values(by="NET PROFIT")
+netprofit["NET PROFIT"] *= 1000  # Multiply the "Net Profit" values by 1000
+fig_netprofit = create_bar_chart(
+    netprofit, netprofit.index, "NET PROFIT", "Net Profit")
+fig_netprofit.update_traces(hovertemplate="<b>Net Profit:</b> %{y}")
+
+# PE Ratio
+
+pe = df_selection.groupby(by=["SYMBOL"]).sum()[["PE"]].sort_values(by="PE")
+fig_pe = create_bar_chart(pe, pe.index, "PE", "PE Ratio")
+fig_pe.update_traces(hovertemplate="<b>PE:</b> %{y}")
+
+# NPL
+
+npl = df_selection.groupby(by=["SYMBOL"]).sum()[["NPL"]].sort_values(by="NPL")
+fig_npl = create_bar_chart(npl, npl.index, "NPL", "NPL")
+fig_npl.update_traces(hovertemplate="<b>NPL:</b> %{y}")
+
+# ROE 
+
+roe = df_selection.groupby(by=["SYMBOL"]).sum()[["ROE"]].sort_values(by="ROE")
+fig_roe = create_bar_chart(roe, roe.index, "ROE", "ROE")
+fig_roe.update_traces(hovertemplate="<b>ROE:</b> %{y}")
+
+# EPS and DPS
+
+eps_dps = df_selection.groupby(by=["SYMBOL"]).sum()[
+    ["EPS", "Dps"]].sort_values(by="EPS")
+fig_eps_dps = go.Figure()
+fig_eps_dps.add_trace(go.Bar(
+    x=eps_dps.index,
+    y=eps_dps["EPS"],
+    name="EPS",
+    marker_color="#0083B8",
+    hovertemplate="<b>EPS:</b> %{y}<extra></extra>",
+    text=eps_dps["EPS"],
+    textposition="auto",
+))
+fig_eps_dps.add_trace(go.Bar(
+    x=eps_dps.index,
+    y=eps_dps["Dps"],
+    name="DPS",
+    marker_color="#FFA15A",
+    hovertemplate="<b>DPS:</b> %{y}<extra></extra>",
+    text=eps_dps["Dps"],
+    textposition="auto",
+))
+fig_eps_dps.update_layout(
+    title="<b>EPS and DPS</b>",
+    template="plotly_white",
+    xaxis_title="Symbol",
+    yaxis_title="Value",
+    legend=dict(x=0.7, y=1),
+
+    # Disable zooming
+    dragmode=False,
+    xaxis=dict(fixedrange=True),
+    yaxis=dict(fixedrange=True)
+)
+# Public Shares
+
+ps = df_selection.groupby(by=["SYMBOL"]).sum(
+)[["Public Shares"]].sort_values(by="Public Shares")
+fig_ps = create_bar_chart(ps, ps.index, "Public Shares", "Public Shares")
+fig_ps.update_traces(hovertemplate="<b>Public Shares:</b> %{y}")
+
+# Display charts
+
+st.plotly_chart(fig_price, use_container_width=True)
+st.plotly_chart(fig_ps, use_container_width=True)
+st.plotly_chart(fig_paid_up, use_container_width=True)
+st.plotly_chart(fig_eps_dps, use_container_width=True)
+st.plotly_chart(fig_bookvalue, use_container_width=True)
+st.plotly_chart(fig_pe, use_container_width=True)
+st.plotly_chart(fig_netprofit, use_container_width=True)
+st.plotly_chart(fig_roe, use_container_width=True)
+st.plotly_chart(fig_npl, use_container_width=True)
+st.plotly_chart(fig_eps, use_container_width=True)
+st.plotly_chart(fig_dps, use_container_width=True
